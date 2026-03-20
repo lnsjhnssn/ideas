5ac280b619f6485ec8d746cd39f391cba0b87c4e
lnsjhnssn <144560735+lnsjhnssn@users.noreply.github.com>
2026-03-20 01:13:31 +0100

    add drifting lights

---
+++ src/components/DriftingLights.jsx
@@ -0,0 +1,28 @@
+import {
+  Shader,
+  FilmGrain,
+  LinearGradient,
+  RectangularCoordinates,
+} from "shaders/react";
+
+export default function DriftingLights() {
+  return (
+    <Shader>
+      <LinearGradient
+        angle={{
+          mode: "loop",
+          type: "auto-animate",
+          speed: 1,
+          easing: "linear",
+          outputMax: 360,
+          outputMin: 0,
+        }}
+        colorA="#212538"
+        colorB="#171b1f"
+        colorSpace="oklab"
+      />
+      <RectangularCoordinates edges="stretch" scale={3} />
+      <FilmGrain strength={0.1} />
+    </Shader>
+  );
+}
